d90d6a354dc499a30fab4b9638c2c8ffd7812511
smuminovic <44192671+smuminovic@users.noreply.github.com>
2018-11-29 17:45:04 +0100

    provjera validnosti imena i prezimena, nekoliko sitnih izmjena u izgledu gui-a

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,33 @@
 package sample;
 
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.TextField;
+
 public class Controller {
+    public TextField imeField;
+    public TextField prezimeField;
+    public TextField indexField;
+    public TextField jmbgField;
+    public TextField datumField;
+    public TextField emailField;
+    public TextField adresaField;
+    public TextField telefonField;
+    public ComboBox mjestoField;
+    public ChoiceBox statusField;
+    public ChoiceBox godinaField;
+    public ChoiceBox ciklusField;
+    public ChoiceBox smjerField;
+    public CheckBox pripadnostField;
+//provjera za ime i prezime
+    private boolean validnoImePrezime(String n) {
+        if (n.length() < 1 || n.length() > 20) return false;
+        for (int i = 0; i < n.length(); i++) {
+            if (!(n.charAt(i) >= 'A' && n.charAt(i) <= 'Z') && !(n.charAt(i) >= 'a' && n.charAt(i) <= 'z'))
+                return false;
+        }
+        return !n.trim().isEmpty();
+    }
+
 }
