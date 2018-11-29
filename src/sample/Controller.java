0eb2981997739d8e5030e27938bb83224d430944
smuminovic <44192671+smuminovic@users.noreply.github.com>
2018-11-29 17:56:41 +0100

    provjera validnosti datuma

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -5,6 +5,9 @@ import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.TextField;
 
+import java.time.DateTimeException;
+import java.time.LocalDate;
+
 public class Controller {
     public TextField imeField;
     public TextField prezimeField;
@@ -20,7 +23,9 @@ public class Controller {
     public ChoiceBox ciklusField;
     public ChoiceBox smjerField;
     public CheckBox pripadnostField;
-//provjera za ime i prezime
+    public String uporediSaJmbg = "";
+    public String datumZaIspis = "";
+    //provjera za ime i prezime
     private boolean validnoImePrezime(String n) {
         if (n.length() < 1 || n.length() > 20) return false;
         for (int i = 0; i < n.length(); i++) {
@@ -29,6 +34,7 @@ public class Controller {
         }
         return !n.trim().isEmpty();
     }
+    //provjera za indeks
     private boolean validanIndeks(String n) {
         if (n.length() > 5) return false;
         for (int i = 0; i < n.length(); i++) {
@@ -37,6 +43,48 @@ public class Controller {
             }
         }
         return !n.trim().isEmpty();
+    }
+    //provjera za datum
+    private boolean validanUnosDatum(String n) {
+
+        return isDateValid(n);
+    }
+    public boolean isDateValid(String s) {
+        if(s.length()<8)return false;
+        int d = 0, m = 0, g = 0;
+        String dan = "", mjesec = "", godina = "";
+        if (s.length() == 8) {
+            dan = s.substring(0, 2);
+            mjesec = s.substring(2, 4);
+            godina = s.substring(4, 8);
+        }
+        else if (s.length() == 10) {
+            if (s.charAt(2) == s.charAt(5) && (s.charAt(2) == '.' || s.charAt(2) == '/' | s.charAt(2) == '-')) {
+                dan = s.substring(0, 2);
+                mjesec = s.substring(3, 5);
+                godina = s.substring(6, 10);
+            }
+        }
+        else {
+            return false;
+        }
+        try {
+            d = Integer.parseInt(dan);
+            m = Integer.parseInt(mjesec);
+            g = Integer.parseInt(godina);
+        }catch (Exception ex){}
+        datumZaIspis=""+ d+"/"+m+"/"+g;
+        int novaG=Integer.parseInt(godina.substring(1,godina.length()));
+        //uporediSaJmbg=""+dan+mjesec+novaG;
+        //System.out.println("Uporedi:"+ uporediSaJmbg);
+        if(d==0 || m==0 || g==0 )return false;
+        boolean dateIsValid = true;
+        try {
+            LocalDate.of(g, m, d);
+        } catch (DateTimeException e) {
+            dateIsValid = false;
+        }
+        return dateIsValid;
     }
 
 }
